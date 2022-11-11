857653b076aee8142240d813f42f055deeee5dfc
Railway <ruiwei687@outlook.com>
2022-11-11 07:40:35 +0700

    update windows stream

--- MauiAudio/NativeAudioService.windows.cs
+++ MauiAudio/NativeAudioService.windows.cs
@@ -69,7 +69,7 @@ public class NativeAudioService : INativeAudioService
     }
     private MediaPlaybackItem mediaPlaybackItem(MediaPlay media)
     {
-        var mediaItem = new MediaPlaybackItem(media.Stream == null ? MediaSource.CreateFromUri(new Uri(media.URL)) : MediaSource.CreateFromStream(media.Stream));
+        var mediaItem = new MediaPlaybackItem(media.Stream == null ? MediaSource.CreateFromUri(new Uri(media.URL)) : MediaSource.CreateFromStream(media.Stream?.AsRandomAccessStream(),string.Empty));
         var props = mediaItem.GetDisplayProperties();
         props.Type = MediaPlaybackType.Music;
         if (media.Name != null) props.MusicProperties.Title = media.Name;
