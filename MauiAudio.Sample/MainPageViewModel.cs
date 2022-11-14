8af97e3815e40d5cf07bf342ee1f3546bb1c0dab
Railway <ruwei687@outlook.com>
2022-11-14 10:41:30 +0700

    update the music; sample: add the play in stream

--- MauiAudio.Sample/MainPageViewModel.cs
+++ MauiAudio.Sample/MainPageViewModel.cs
@@ -12,7 +12,7 @@ public partial class MainPageViewModel
 {
     PlayerService playerService;
     [ObservableProperty]
-    string url= "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3";
+    string url= "https://dkihjuum4jcjr.cloudfront.net/ES_ITUNES/Thunderbird/ES_Thunderbird.mp3";
     public MainPageViewModel(PlayerService player)
     {
         playerService = player;
@@ -21,5 +21,11 @@ public partial class MainPageViewModel
     void Play()
     {
         playerService.PlayAsync(new() { URL = Url, Name = "TempUrl",Author="TempAuthor" });
+    }
+    [RelayCommand]
+    async void PlayInStream()
+    {
+        var stream = await FileSystem.OpenAppPackageFileAsync("sample.mp3");
+        await playerService.PlayAsync(new() { Stream=stream, Name = "TempUrl", Author = "TempAuthor" });
     }
 }
